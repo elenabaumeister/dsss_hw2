495edf811a299d4f68ec6498daa0fcdd7bd058ad
Elena <elena.baumeister@fau.de>
2024-11-10 20:53:12 +0100

    file adjusted to homework tasks

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,105 @@
 import random
 
 
-def function_A(min, max):
+def random_integer(lower_boundary, upper_boundary):
     """
-    Random integer.
+    Random integer in range of min to max is returned.
+
+    Parameters
+    ----------
+    lower_boundary : int
+        Lower bound of the random integer.
+    upper_boundary : int
+        Upper bound of the random integer.
+
+    Returns
+    -------
+    int
+        Random integer in the range of min to max.
     """
-    return random.randint(min, max)
+    try:
+        return random.randint(lower_boundary, upper_boundary)
+    except ValueError:
+        print("Please enter valid integers for the range.")
+    except TypeError:
+        print("Please enter integers for the range.")
 
 
-def function_B():
+def random_sign():
+    """
+    Randomly '+', '-', '*' sign is returned.
+
+    Returns
+    -------
+    str
+        Random sign from the list ['+', '-', '*'].
+
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculation(int1, int2, sign):
+
+    """
+    Mathematical problem and its answer are calculated.
+
+    Parameters
+    ----------
+    int1 : int
+        First random integer of the calculation.
+    int2 : int
+        Second random integer of the calculation.
+    sign : str
+        Random sign of the calculation.
+
+    Returns
+    -------
+    calculation_string : str
+        Formula of the math problem.
+    result : int
+        Result of the math problem.
+    """
+    calculation_string = f"{int1} {sign} {int2}"
+    if sign == '+':
+        result = int1 + int2
+    elif sign == '-':
+        result = int1 - int2
+    else:
+        result = int1 * int2
+    return calculation_string, result
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Math quiz game is played. User is asked to solve math problems and the score is calculated.
+    """
+    points = 0  # initialize points
+    t_q = 3  # number of total questions
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        # two random integers and a random sign are generated
+        int_1 = random_integer(1, 10)
+        int_2 = random_integer(1, 5)
+        sign = random_sign()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
+        # formula and result of the math problem are calculated
+        formula, result = calculation(int_1, int_2, sign)
+        print(f"\nQuestion: {formula}")
+        # user can enter its answer
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
-        if useranswer == ANSWER:
+        if useranswer == result:
             print("Correct! You earned a point.")
-            s += -(-1)
+            points += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {result}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {points}/{t_q}")
+
 
 if __name__ == "__main__":
     math_quiz()
