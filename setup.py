e539e19fbb94d6ce7f5e23a43defb1a496bf2275
Elena <elena.baumeister@fau.de>
2024-11-10 21:24:30 +0100

    adjustments

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name="snowflake",
+    name="math_quiz",
     version="0.1",
     author="DSSS",
-    author_email="luisa.e.neubig@fau.de",
+    author_email="elena.baumeister@fau.de",
     packages=find_packages(),
-    install_requires=["numpy", "turtles"],
+    install_requires=["math_quiz"],
 )
